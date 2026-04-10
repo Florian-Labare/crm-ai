4022cf1915b7d85e18089381776a5a021ededfc6
florianlabare <florian.labare@bwagence.fr>
2026-04-10 08:49:38 +0200

    fix dette technique

--- frontend/eslint.config.js
+++ frontend/eslint.config.js
@@ -19,5 +19,13 @@ export default defineConfig([
       ecmaVersion: 2020,
       globals: globals.browser,
     },
+    rules: {
+      '@typescript-eslint/no-explicit-any': 'warn',
+      '@typescript-eslint/no-unused-vars': 'warn',
+      '@typescript-eslint/no-unused-expressions': 'warn',
+      'no-empty': 'warn',
+      'prefer-const': 'warn',
+      'react-refresh/only-export-components': 'warn',
+    },
   },
 ])
